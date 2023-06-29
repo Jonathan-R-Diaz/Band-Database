399974d8f2ffa26b2bc15f42be96581eb94bdc3c
Jonathan Diaz <jrd17j@fsu.edu>
2023-06-29 14:13:55 -0400

    Added dividers and scrollable list

--- java/com/zybooks/thebanddatabase/ListFragment.java
+++ java/com/zybooks/thebanddatabase/ListFragment.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
 public class ListFragment extends Fragment {
@@ -16,38 +18,73 @@ public class ListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_list, container, false);
-        LinearLayout layout = (LinearLayout) rootView;
 
-        // Create the buttons using the band names and ids from BandRepository
-        List<Band> bandList = BandRepository.getInstance(requireContext()).getBands();
-        for (Band band : bandList) {
-            Button button = new Button(getContext());
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT);
-            layoutParams.setMargins(0, 0, 0, 10);   // 10px bottom margin
-            button.setLayoutParams(layoutParams);
+        // Click listener for the RecyclerView
+        View.OnClickListener onClickListener = itemView -> {
 
-            // Display band's name on button
-            button.setText(band.getName());
-            button.setTag(band.getId());
+            // Create fragment arguments containing the selected band ID
+            int selectedBandId = (int) itemView.getTag();
+            Bundle args = new Bundle();
+            args.putInt(DetailFragment.ARG_BAND_ID, selectedBandId);
 
-            // Navigate to detail screen when clicked
-            button.setOnClickListener(buttonView -> {
+            // Replace list with details
+            Navigation.findNavController(itemView).navigate(R.id.show_item_detail, args);
+        };
 
-                // Create fragment arguments containing the selected band ID
-                int selectedBandId = (int) buttonView.getTag();
-                Bundle args = new Bundle();
-                args.putInt(DetailFragment.ARG_BAND_ID, selectedBandId);
+        // Send bands to RecyclerView
+        RecyclerView recyclerView = rootView.findViewById(R.id.band_list);
+        List<Band> bands = BandRepository.getInstance(requireContext()).getBands();
+        recyclerView.setAdapter(new BandAdapter(bands, onClickListener));
 
-                // Send band ID to DetailFragment
-                Navigation.findNavController(buttonView).navigate(R.id.show_item_detail, args);
-            });
-
-            // Add button to the LinearLayout
-            layout.addView(button);
-        }
+        DividerItemDecoration divider = new DividerItemDecoration(recyclerView.getContext(),
+                DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(divider);
 
         return rootView;
+    }
+
+    private class BandAdapter extends RecyclerView.Adapter<BandHolder> {
+
+        private final List<Band> mBands;
+        private final View.OnClickListener mOnClickListener;
+
+        public BandAdapter(List<Band> bands, View.OnClickListener onClickListener) {
+            mBands = bands;
+            mOnClickListener = onClickListener;
+        }
+
+        @NonNull
+        @Override
+        public BandHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            return new BandHolder(layoutInflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(BandHolder holder, int position) {
+            Band band = mBands.get(position);
+            holder.bind(band);
+            holder.itemView.setTag(band.getId());
+            holder.itemView.setOnClickListener(mOnClickListener);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mBands.size();
+        }
+    }
+
+    private static class BandHolder extends RecyclerView.ViewHolder {
+
+        private final TextView mNameTextView;
+
+        public BandHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_band, parent, false));
+            mNameTextView = itemView.findViewById(R.id.band_name);
+        }
+
+        public void bind(Band band) {
+            mNameTextView.setText(band.getName());
+        }
     }
 }
